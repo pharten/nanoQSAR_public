a402829a5c196af313858101195966f036d20af8
pharten <harten.paul@epa.gov>
2023-02-22 15:40:11 -0500

    Compare sklearn's KFold cross validation with cross_validation_score()

--- Python_Random_Forest/src/Validation.py
+++ Python_Random_Forest/src/Validation.py
@@ -85,7 +85,9 @@ def main():
         print(" r2_train = %f, r2_test = %f" % (r2_train[index], r2_test[index] ))
         index += 1
 
-    print(" r2_train = %f +/- %f, r2_test = %f +/- %f" % (r2_train.mean(), r2_train.std(), r2_test.mean(), r2_test.std() ))
+    print(" r2_train = %f +/- %f, r2_test = %f +/- %f" % (r2_train.mean(), r2_train.std(), r2_test.mean(), r2_test.std()))
+    
+    print ("\n",cross_val_score(rfa, X, y))
 
     """
     rfa.fit(X, y)
